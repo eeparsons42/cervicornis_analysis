3f17e28c2057f59e08daa969afe43e8d3f837370
Emily Parsons <parsonsee@g.cofc.edu>
2020-03-10 10:36:34 -0400

    reformatted to rscript - data divided by out/self crosses

--- scripts/raw_data_initial_lme.R
+++ scripts/raw_data_initial_lme.R
@@ -1,57 +1,58 @@
----
-  title: "Acer analysis"
-author: "Emily Parsons"
-date: "March 9, 2020"
-output: html_document
----
-  
-  ```{r}
+library(nlme)
+library(ggplot2)
+
 raw.counts <- read.csv("C:/Users/Emily/Desktop/acer.all.csv")
 head(raw.counts)
-```
 
-```{r}
+# code crosses
+
+same_letter <- function(x) {
+  x_split <- strsplit(as.character(x), "")
+  if (x_split[[1]][1] == x_split[[1]][2])
+    return(TRUE)
+  else 
+    return(FALSE)
+}
+
+selfs <- sapply(raw.counts$Sample.ID, same_letter)
+
+raw.counts$cross <- ifelse(selfs, 'self', 'out')
+
+
 boxplot(raw.counts$Fert.rate~raw.counts$Night)
-```
 
-```{r}
-install.packages("nlme")
-```
-```{r}
-library(nlme)
-```
+ggplot(raw.counts, aes(Sample.ID, Fert.rate)) + 
+  geom_boxplot(aes(col = Night))
 
 
-```{r}
-acer.lm <- lm(Fert.rate~Night, data = raw.counts)
+acer.lm <- lm(Fert.rate ~ Night + Sample.ID , data = raw.counts)
 summary(acer.lm)
-```
 
 
-```{r}
-acer.lme <- lme(Fert.rate~Night, random = ~ 1 | Sample.ID, data=raw.counts)
+acer.lme <- lme(Fert.rate ~ Night, random = ~ 1 | Sample.ID, data=raw.counts)
 summary(acer.lme)
-```
 
-```{r}
+head(raw.counts)
+
 plot(acer.lme)
-```
 
-Plot of standardized residuals - here we see a systematic increase in the variance of the Eij as the level of the response increases (the residuals on the right-had side of the plot have a greater vertical spread than those on the left, forming a wedge-shaped pattern) 
 
-```{r}
+#Plot of standardized residuals - here we see a systematic increase in the variance of... 
+#the Eij as the level of the response increases...
+#(the residuals on the right-had side of the plot have a greater vertical spread than those on the left,... 
+#forming a wedge-shaped pattern)...
+
+
 intervals(acer.lme)
-```
-```{r}
+
 anova(acer.lme)
-```
 
 
-```{r}
+
+
 means<- aggregate(raw.counts[,6], by = list(raw.counts[,2]), FUN = mean)
 head(means)
-```
 
-```{r}
+
+
 barplot(means$x, main = "Fert rates")
-```
